53fa784246541cba611986907c383f41c68ce7bd
JaeHyeong <153871446+superjutgal11@users.noreply.github.com>
2024-02-08 00:25:50 +0900

    24.02.07

---
+++ 24.02.07.py
@@ -0,0 +1,143 @@
+# 정수계산부터 복습
+
+'''
+# a = 10
+# b = 2
+# c = 3
+
+# print(a+b+c)
+# print(a-b-c)
+# print(a*b)
+# print(a/c)
+# print(a**c)
+# print(pow(a,c))
+# print(a//c)
+# print(a%c) 
+# print(divmod(a,c))
+
+
+# a = 102
+
+# print(bin(a))
+# print(oct(a))
+# print(hex(a))
+
+# print(0b1101)
+# print(0o1101)
+# print(0x1101)
+# print(0X1101)
+
+# a = 10.3
+# b = 10.8
+
+# print(type(a))
+# print(int(a))
+# print(int(b))
+# a = int(a)
+# print(type(a))
+
+# print(3.5+2.2) # 실수끼리의 덧셈
+# print(5+4.3) # 정수와 실수의 덧셈
+# print(4.3-2.7) # 실수끼리의 뺄셈
+
+# print(2.23+9.00012)
+
+# a = 102
+# a = float(a)
+# print(a)
+# print(type(a))
+# print(float(10+2))
+
+# a1 , a2 , a3 , a4 = 10 , 20 , 20 , None ; print(a1,a2,a3,a4)
+
+# b1 = b2 = b3 = b4 = 10
+# print(b1,b2,b3,b4)
+
+# a = 10 ; b = 20
+# print(a,b)
+# del b
+# print(a,b)
+
+# a = input("정수1을 입력하세요 : ")
+# b = input("정수2를 입력하세요 : ")
+# print(a+b)
+# a2 = int(input("정수3을 입력하세요 : "))
+# b2 = int(input("정수4을 입력하세요 : "))
+# print(a2+b2)
+
+# a , b , c , d = input("이름,학교,지역,나이를 띄어쓰기로 구분하여 입력하시오.").split()
+# print("이름은 {0}이고 {1}지역의 {2}대학을 다니고 있으며 나이는 {3}입니다.".format(a,c,b,int(d)))
+
+# a,b,c = input("값 입력 : ").split("/")
+# print(a,b,c)
+
+# 파이썬 너무 재밌당
+
+# a , b , c = map(int,input("값 3개를 ,로 구분하여 입력 : ").split(","))
+# print(a+b+c)
+
+# a , b = 2 , 3
+# print(1,2,3,4)
+# print(a,b)
+
+# a,b,c=10,20,30
+# print(a,b,c,5,10,sep='\n')
+
+# print(10)
+# print(20)
+# print(30)
+
+# print(10,end='')
+# print(20,end='')
+# print(30)
+
+# print(10,end='!!')
+# print(20,end='!!')
+# print(30)
+
+# a,b,c,d = input("공백 2칸으로 구분하여 개인정보를 작성하시오.\n이름,성별,출신지,대학교 : ").split("  ")
+# print("이름:{0},출신지:{1},출신대학:{2},성별:{3}".format(a,c,d,b))
+
+# print("데이터베이스를 입력해 주십시오",end="     ")
+# print("주민앞자리/나이/잔화반호")
+# a,b,c,d=input("구분은 / 로 합니다. : ").split("/")
+# print(a,b,c,d)
+
+# print(10==10)
+# print(10!=10)
+# print(10<20)
+# print(10<=10)
+# print(10>=20)
+# print(10>=10)
+
+# a = '안녕하세요'
+# b = "안녕하세요"
+# print(a==b,a!=b,sep=" // ")
+
+# a = 10.0
+# b = 10
+# print(a==b)
+# print(a is b)
+# print(id(a),id(b))
+'''
+
+# # print(bool(1))
+# # print(bool(-10.2))
+# # print(bool(0))
+# # print(bool('False'))
+# # print(bool(False))
+
+# print(True and True)
+# print(not True or False)
+# print(not 10 >= 20 and 200 != 100)
+
+# print('''안녕하세요''')
+# print("""반갑습니다""")
+# print('''저는
+# 정재형이라고
+# # 합니다''')
+
+# print('내 이름은 "정재형"이야.')
+# print("Phython isn't difficult.")
+# print("나는 어제 '마라탕'을 처음 \"박의진\"이라는 친구랑 먹었어.")
+
