f5c4f317c9c517ea37f707377bc140906ba1e0df
JaeHyeong <153871446+superjutgal11@users.noreply.github.com>
2024-02-08 22:28:21 +0900

    24.02.08

---
+++ 24.02.08.py
@@ -0,0 +1,89 @@
+# 리스트
+
+# a = [10,20,30,40]
+# print(a)
+
+# b = [True,20.001,-10,"jjh"]
+# print(b)
+
+# c = [] ; d = list()
+# print(c,d,sep="   ")
+
+# print(range(10))
+
+# a = list(range(10))
+# print(a)
+# b = list(range(10,20))
+# print(b)
+# c = list(range(10,0,-1))
+# print(c)
+
+# a = (10,20,30,0,40)
+# b = -10,2.3,"jjh",True,40
+# print(a,b,sep="     ")
+
+# c = 3,
+# d = 3
+# print(type(c),type(d))
+
+
+# a = tuple(range(10))
+# print(a)
+
+# b = tuple(range(20,25))
+# print(b)
+
+# c = tuple(range(1,12,2))
+# print(c)
+
+# a = [10,20,30]
+# b = (1,2,3)
+# c = tuple(b)
+# d = list(a)
+# print(c,d,sep="      ")
+
+# print(list("정재형"))
+# print(tuple("Jeong Jae Hyeong"))
+
+# a,b,c = ["정재형",25,'JJH']
+# d,e,f = 10,20,30
+# print(a,b,c,"\n",d,e,f)
+
+# x = [1,2,3]
+# y = "jjh",10,0.01,"x"
+# a1,a2,a3 = x
+# b1,b2,b3,b4 = y
+# print(x,y,sep="     ")
+
+# a = input("10 20 30을 입력 : ").split()
+# print(a)
+
+# a=input("값 3개를 공백 1칸으로 구분하여 입력 : ").split()
+# b,c,d=a
+# print(b,c,d)
+
+
+# a = 1,2,3,4,5,6,7
+# print(1 in a)
+# print(10 in a)
+# print(10 not in a)
+
+# a = [1,2,3,4]
+# b = [5,6,7]
+# print(a+b)
+# c = 8,9
+# print(tuple(a)+tuple(b)+c)
+# print(str(25)+"살을 먹었습니다.")
+# print(c+tuple(range(5)))
+
+# a = ["dddd",10,10.002]
+# print(a*3) # 정수만큼만 곱할 수 있음
+
+# b = (10,10,10,10,2)
+# c = b * 2 + tuple(range(1,5,1))
+# print(c)
+
+# d = "gyeongsang"
+# print(d*3)
+
+# print(type(None))
