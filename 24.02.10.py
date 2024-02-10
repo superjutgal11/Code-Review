2c8532217c24c524d0a68b897ccc250777df22ac
JaeHyeong <153871446+superjutgal11@users.noreply.github.com>
2024-02-10 12:52:41 +0900

    24.02.10

---
+++ 24.02.10.py
@@ -0,0 +1,56 @@
+# a = [10,20.33,"jjh"]
+# b = 5,5,5,4,4,1
+# c = "Welcome to Python"
+
+# print(len(a)) # 요소개수 총 3개
+# print(len(b)) # 요소개수 총 6개
+# print(len(c)) # 요소개수(띄어쓰기 포함) 총 17개
+# print(len(range(1,5,1))) # 1,2,3,4 총 4개
+
+
+# # 
+# a = [1,2,3,4,5,6,7,8,9,10]
+# b = "제 이름은 정재형 입니다."
+
+# print(a[0],a[1],a[9],sep="     ")
+# print(b[0],b[4],b[10],sep="     ")
+# print(type(b[0]),type(b[4]),type(b[10]),sep="     ")
+
+# a = [1,2,3,4,5,6,7,8,9,10]
+
+# print(a) # [인덱스]가 없으면 시퀀스객체의 요소 전체에 접근
+# print(a.__getitem__(2))
+
+# a = [1,2,3,4,5]
+# print(a[-1],a[-5],sep="     ")
+
+# # print(a[5])
+# # print(a[-6])
+
+# print(a[-1],a[len(a)-1],sep="     ")
+
+# z = [0,0,0,0]
+
+# z[1] = 50
+# z[3] = 60
+# print(z)
+
+# # y = "helly"
+# # y[5] = "o"
+# # print(y)
+
+# x = ["jjh",20,-3.5]
+# del x[0]
+# print(x)
+
+
+# list_1 = [10,20,'jjh',10]
+# list_1[4] = "kr"
+# print(list_1)
+
+
+# slice = [ 0,10,20,30,40,50,60,70,80,90,100 ]
+
+# print(slice[1:5])
+
+# print(slice[3:-4])
